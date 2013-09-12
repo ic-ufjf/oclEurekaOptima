72bcd3168d098d5aae38c6bedd9449ee4aec0802
igorsr <igor.souza.russo@gmail.com>
2013-09-12 20:34:44 -0300

    Testes do AG

--- trunk/src/operadores_genericos.h
+++
@@ -1,8 +0,0 @@
-#ifndef OPERADORES_GENETICOS_H_INCLUDED
-#define OPERADORES_GENETICOS_H_INCLUDED
-
-void mutacao(char* individuo, double chance);
-void crossover_um_ponto(char *pai1, char*pai2, char*filho1, char*filho2);
-void recombinacao(char *pai1, char *pai2, char *filho1, char *filho2, float chance);
-
-#endif // OPERADORES_GENETICOS_H_INCLUDED
