479b76363df4d3f863f94c7cac22c56273e9cf0c
igorsr <igor.souza.russo@gmail.com>
2014-05-14 15:41:32 -0300

    avaliação implementada recompilando os modelos candidatos

---
+++ trunk/src/ocl/EG_avaliacao_paralela_compilador/representacao.h
@@ -0,0 +1,13 @@
+#ifndef REPRESENTACAO_H_INCLUDED
+#define REPRESENTACAO_H_INCLUDED
+
+#include "parametros_ag.h"
+
+typedef struct{
+
+    short genotipo[TAMANHO_INDIVIDUO];
+    float aptidao;
+
+} individuo;
+
+#endif // REPRESENTACAO_H_INCLUDED
